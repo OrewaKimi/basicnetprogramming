6f7575b336b692118d34048868093b1ca33001ab
Kimi Maulana <kimi93.com@gmail.com>
2024-09-21 12:06:07 +0700

    Update app.js (Argumen Parsing)

--- PBJ2/app.js
+++ PBJ2/app.js
@@ -1,14 +1,64 @@
-const ambilCatatan = require('./catatan.js');  
-const command = process.argv[2];  // Mengambil argumen ketiga dari command line
+const yargs = require('yargs');
+const catatan = require('./catatan.js');
 
-console.log(process.argv);
+// Kustomisasi versi yargs
+yargs.version('10.1.0');
 
-if (command === 'tambah') {
-    console.log('Tambah Catatan');
-    const pesan = ambilCatatan(); 
-    console.log(pesan);  
-} else if (command === 'hapus') {
-    console.log('Hapus Catatan');
-} else {
-    console.log('Perintah tidak dikenali.');
-}
+// Membuat perintah (command) 'tambah'
+yargs.command({
+    command: 'tambah',
+    describe: 'tambah sebuah catatan baru',
+    builder: {
+        judul: {
+            describe: 'Judul catatan',
+            demandOption: true,
+            type: 'string'
+        },
+        isi: {
+            describe: 'Isi catatan',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Judul: ' + argv.judul);
+        console.log('Isi: ' + argv.isi);
+    }
+});
+
+// Perintah hapus
+yargs.command({
+    command: 'hapus',
+    describe: 'hapus catatan',
+    handler: function () {
+        console.log('Catatan berhasil dihapus');
+    }
+});
+
+// Perintah untuk menampilkan semua catatan
+yargs.command({
+    command: 'list',
+    describe: 'Menampilkan semua catatan',
+    handler: function () {
+        console.log('Menampilkan semua catatan');
+    }
+});
+
+// Perintah untuk membaca catatan
+yargs.command({
+    command: 'baca',
+    describe: 'Membaca sebuah catatan',
+    builder: {
+        judul: {
+            describe: 'Judul catatan yang ingin dibaca',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Membaca catatan dengan judul: ' + argv.judul);
+    }
+});
+
+// Parsing yargs
+yargs.parse();
