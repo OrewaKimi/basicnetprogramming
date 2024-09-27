ef0646550df9a4c7a15801ac47ff793db405640b
Kimi Maulana <kimi93.com@gmail.com>
2024-09-27 14:40:50 +0700

    Update C. Latihan 1- API Access Weatherstack

--- PBJ3/cekCuaca.js
+++ PBJ3/cekCuaca.js
@@ -1,20 +1,22 @@
 const request = require('postman-request');
 
 // Mengubah ke Fahrenheit dan menggunakan bahasa Inggris
-const urlCuaca = 'http://api.weatherstack.com/current?access_key=9171b5925ca219298c4874b016ae6a6b&query=-0.8969206647459452,100.35076508271707&units=f&language=en';
+const urlCuaca = 'http://api.weatherstack.com/current?access_key=9171b5925ca219298c4874b016ae6a6b&query=-0.8969206647459452,100.35076508271707&units=f';
 
 request({ url: urlCuaca, json: true }, (error, response) => {
     if (error) {
-        console.log('Tidak dapat menghubungi layanan cuaca!', error);
+        console.log('Tidak dapat menghubungi layanan cuaca! Pastikan Anda terhubung ke internet.');
+        console.log('Error:', error);
     } else if (response.body.error) {
         console.log('Error dari API: ', response.body.error.info);
     } else {
-        const temperature = response.body.current.temperature;
-        const rainChance = response.body.current.precip;
-        const weatherDescription = response.body.current.weather_descriptions[0]; // deskripsi cuaca
+        const temperature = response.body.current.temperature; // Suhu 
+        const rainChance = response.body.current.precip; // Kemungkinan hujan
+        const weatherDescription = response.body.current.weather_descriptions[0]; // Deskripsi cuaca
 
-        console.log('Current temperature is ' + temperature + ' degrees Fahrenheit.');
-        console.log('Chance of rain is ' + rainChance + '%.');
-        console.log('Weather description: ' + weatherDescription + '.');
+        // Tampilkan informasi cuaca
+        console.log(`Saat ini suhu di luar mencapai ${temperature} derajat Fahrenheit.`);
+        console.log(`Kemungkinan terjadinya hujan adalah ${rainChance}%.`);
+        console.log(`Deskripsi cuaca: ${weatherDescription}.`);
     }
 });
