4854bbd5b57b334ec0df039030289cdc718e96bb
Kimi Maulana <kimi93.com@gmail.com>
2024-09-27 14:42:46 +0700

    Update Komen C. Latihan 1- API Access Weatherstack

--- PBJ3/cekCuaca.js
+++ PBJ3/cekCuaca.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 
-// Mengubah ke Fahrenheit dan menggunakan bahasa Inggris
+// Mengubah ke Fahrenheit 
 const urlCuaca = 'http://api.weatherstack.com/current?access_key=9171b5925ca219298c4874b016ae6a6b&query=-0.8969206647459452,100.35076508271707&units=f';
 
 request({ url: urlCuaca, json: true }, (error, response) => {
